75a0487ab42a3463481821076130294398f8e627
parsaravaghi <parasa.ravaghi@gmail.com>
2024-09-14 22:37:34 +0330

    create product table

--- server/app/Http/Middleware/CheckJwt.php
+++ server/app/Http/Middleware/CheckJwt.php
@@ -20,29 +20,6 @@ class CheckJwt
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // skiping when request method is get
-        if(request()->method() == "GET" ) return $next($request);
-
-        // getting public key from env file
-        $key = env("public_key");
-
-        $wrongMessage = new JsonResponse( ["message" => "jwt token is wrong"]  , 403);
-
-        try 
-        {
-            // checking for is jwt header valid or not
-            $jwt_result = JWT::decode($request->header("jwt-token"), $key, array('HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512'));
-        } 
-        catch (\Throwable $th) 
-        {
-            Cookie::forget("jwt-token");
-            Cookie::queue( Cookie::forget("jwt-token") );
-            $token = JWT::encode([ "userId" =>null] , $key );
-            Cookie::queue(Cookie::make("jwt-token" , $token , 500));
-
-            return $wrongMessage;
-        }
-
         return $next($request);
     }
 }
