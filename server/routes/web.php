f1e565ae7799c07e2394e1bba36595715415a2a4
parsa <parsadox4@gmail.com>
2024-10-01 20:42:43 +0330

    Delete server directory

--- server/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Authentication;
-use App\Http\Middleware\isAuthenticated;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/register', [Authentication::class , 'Register']);
-Route::post('/login', [Authentication::class , 'Login'])->middleware([isAuthenticated::class]);
-Route::post('/logout', [Authentication::class , 'Logout']);
